82461f5dc5c6c7f3d18b5aa7040a5e0c39960ead
Ali C <98207376+cxali@users.noreply.github.com>
2024-01-20 22:00:16 +0800

    Create echo_chatbot.py
    In this project, I applied my knowledge of:
    variables
    string concatenation
    output
    user input
    to create “Echo Chatbot,” a chatbot that echoes whatever a user inputs.

---
+++ echo_chatbot.py
@@ -0,0 +1,4 @@
+print("Welcome!")
+print("Please feel free to talk whatever you want to. What you say will be repeated by the chatbot.")
+stuff_to_echo = input("Enter something for me to repeat: ")
+print("You said: " + stuff_to_echo)
